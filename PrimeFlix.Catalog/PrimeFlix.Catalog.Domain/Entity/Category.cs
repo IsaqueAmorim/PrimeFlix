8b1cd961d7220b09951e1446ac337916db669cc9
Isaque Amorim <isaque.amorim@inventsoftware.com.br>
2023-12-08 08:35:11 -0300

    :sparkles: creating category entity with tests

---
+++ PrimeFlix.Catalog/PrimeFlix.Catalog.Domain/Entity/Category.cs
@@ -0,0 +1,32 @@
+﻿using PrimeFlix.Catalog.Domain.Exceptions;
+
+namespace PrimeFlix.Catalog.Domain.Entity
+{
+    public class Category
+    {
+        public Category(string name, string description, bool isActive = true)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Description = description;
+            CreatedAt = DateTime.Now;
+            IsActive = isActive;
+
+            Validate();
+        }
+
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public bool IsActive { get; private set; }
+
+        public void Validate()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+                throw new EntityValidationException($"{nameof(Name)} should not be empty or null");
+            if (Description is null) 
+                throw new EntityValidationException($"{nameof(Description)} should not be empty or null");
+        }
+    }
+}
---
+++ PrimeFlix.Catalog/PrimeFlix.Catalog.UnitTests/Domain/CategoryTests.cs
@@ -0,0 +1,80 @@
+﻿using PrimeFlix.Catalog.Domain.Entity;
+using PrimeFlix.Catalog.Domain.Exceptions;
+using Xunit;
+
+namespace PrimeFlix.Catalog.UnitTests.Domain
+{
+    public class CategoryTests
+    {
+        const string NAMESPACE = "Domain";
+        const string CATEGORY_GROUP = "Category - Aggregates";
+
+        [Fact(DisplayName = nameof(CategoryInstance_ShouldCreate_NewCategory))]
+        [Trait(NAMESPACE, CATEGORY_GROUP)]
+        public void CategoryInstance_ShouldCreate_NewCategory()
+        {
+            //ARRANGE
+            var validData = new
+            {
+                Name = "Ação",
+                Description = "Uma Descrição"
+            };
+
+            //ACT
+            var datetimeBefore = DateTime.Now;
+            var category = new Category(validData.Name, validData.Description);
+            var datetimeAfter = DateTime.Now;
+
+            //ASSERT
+            Assert.NotNull(category);
+            Assert.Equal(validData.Name, category.Name);
+            Assert.Equal(validData.Description, category.Description);
+            Assert.NotEqual(default(Guid), category.Id);
+            Assert.NotEqual(default(DateTime), category.CreatedAt);
+            Assert.True(category.CreatedAt > datetimeBefore);
+            Assert.True(category.CreatedAt < datetimeAfter);
+            Assert.True(category.IsActive);
+        }
+
+        [Theory(DisplayName = nameof(CategoryInstanceWithParameter_ShouldCreate_NewCategory))]
+        [Trait(NAMESPACE, CATEGORY_GROUP)]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void CategoryInstanceWithParameter_ShouldCreate_NewCategory(bool isActive)
+        {
+            //ARRANGE
+            var validData = new
+            {
+                Name = "Ação",
+                Description = "Uma Descrição"
+            };
+
+            //ACT
+            var datetimeBefore = DateTime.Now;
+            var category = new Category(validData.Name, validData.Description,isActive);
+            var datetimeAfter = DateTime.Now;
+
+            //ASSERT
+            Assert.NotNull(category);
+            Assert.Equal(validData.Name, category.Name);
+            Assert.Equal(validData.Description, category.Description);
+            Assert.NotEqual(default(Guid), category.Id);
+            Assert.NotEqual(default(DateTime), category.CreatedAt);
+            Assert.Equal(category.IsActive, isActive);
+            Assert.True(category.CreatedAt > datetimeBefore);
+            Assert.True(category.CreatedAt < datetimeAfter);
+        }
+
+        [Theory(DisplayName = nameof(CategoryIntaceWithEmptyName_ShouldCreate_ThrowExeption))]
+        [Trait("Domain", "Category - Aggregates")]
+        [InlineData(null)]
+        [InlineData("   ")]
+        [InlineData("")]
+        public void CategoryIntaceWithEmptyName_ShouldCreate_ThrowExeption(string name)
+        {
+            Action action = () => new Category(name, "Descrição");
+
+            var exception = Assert.Throws<EntityValidationException>(action);
+        }
+    }
+}
